1f60c47ab82f746d1d1cc553474b1628ace116a4
Sam Khong Lucas RASAMIZANANY <Sam-khong-lucas.rasamizanany@ens.fr>
2021-04-27 10:14:17 +0200

    modifications sur l'interface graphique uniquement, changement du grid en place ça m'a cassé les couilles mdr

--- projet.py
+++ projet.py
@@ -28,9 +28,10 @@ liste_couleurs = ["red", "blue", "green", "pink","orange", "yellow", "white"]
 liste_mots = ["Rouge", "Bleu", "Vert", "Rose","Orange", "Jaune", "Blanc"]
 temps_ecoule = ''
 mot = tkFont.Font(family='Baskerville Old Face', size=18, weight='bold')
-texte = tkFont.Font(family='Baskerville Old Face', size=15)
-score_temps = tkFont.Font(family='Baskerville Old Face', size=14)
+texte = tkFont.Font(family='Baskerville Old Face', size=18)
+score_temps = tkFont.Font(family='Baskerville Old Face', size=16)
 bouton = tkFont.Font(family='Arial Black', weight='bold', size=9)
+demarrer_reinitialiser = tkFont.Font(family='Baskerville Old Face', size=13)
 
 #################################################################
 #fonctions
@@ -39,7 +40,7 @@ def temps_restant():
     """ Fait le décompte du temps imparti (30 secondes) """
     global cpt_temps, temps_ecoule
     bouton_demarrer["state"] = "disabled"  #desactivation du bouton démarrer
-    message_temps.configure(text="Temps restant: " + str(cpt_temps-1))
+    message_temps.configure(text="Temps restant: " + str(cpt_temps-1) + 's')
     cpt_temps -= 1
     temps_ecoule = racine.after(1000, temps_restant)
     
@@ -57,7 +58,7 @@ def reinitialiser():
     racine.after_cancel(temps_ecoule)
     bouton_demarrer['state'] = 'normal'
     cpt_temps = 30
-    message_temps.configure(text="Temps restant: " + str(cpt_temps))
+    message_temps.configure(text="Temps restant: " + str(cpt_temps) + 's')
 
     #partie score
     cpt_score = 0
@@ -66,43 +67,65 @@ def reinitialiser():
 
 ################################################################################################
 #création et positionnement des widgets
+#création des widgets
 
-#messages d'informations
-texte1 = tk.Label(text=" Tapez la couleur des mots, pas le texte des mots ! ! !", font=texte, bg='gray84')
-texte1.grid(row=0, column=0, padx=50, pady=10, columnspan=5)
-
-message_score = tk.Label(text="Score: " + str(cpt_score), font=score_temps, bg='gray84')
-message_score.grid(row=1, column=0, columnspan=5)
-
-message_temps = tk.Label(text="Temps restant: " + str(cpt_temps), font=score_temps, bg='gray84')
-message_temps.grid(row=2, column=0, columnspan=5)
+texte1 = tk.Label(text=" Tapez la couleur des mots, pas le texte des mots ! ! !",
+                font=texte, bg='gray84')
+message_score = tk.Label(text="Score: " + str(cpt_score), font=score_temps,
+                bg='gray84')
+message_temps = tk.Label(text="Temps restant: " + str(cpt_temps) + 's',
+                font=score_temps, bg='gray84')
 
 #zone d'affichage des mots
 mots = tk.Label(text= "MOTS EN COULEURS",font=mot, fg="blue", bg='gray84')
-mots.grid(row=3, column=0, pady=30, columnspan=5, sticky="we")
 
 #boutons couleurs
-bouton_rouge  = tk.Button(text="Rouge", font=bouton, bg="red", bd=0, height=3, width= 15)
-bouton_bleu   = tk.Button(text="Bleu", font=bouton, bg="blue", bd=0, height=3, width= 15)
-bouton_vert   = tk.Button(text="Vert", font=bouton, bg="green", bd=0, height=3, width= 15)
-bouton_rose   = tk.Button(text="Rose", font=bouton, bg="pink", bd=0, height=3, width= 15)
-bouton_orange = tk.Button(text="Orange", font=bouton, bg="orange", bd=0, height=3, width= 15)
-bouton_jaune  = tk.Button(text="Jaune", font=bouton, bg="yellow", bd=0, height=3, width= 15)
-bouton_blanc  = tk.Button(text="Blanc", font=bouton, bg="white", bd=0, height=3, width= 15)
-
-bouton_rouge.grid(row=4 ,column=0, padx=8)
-bouton_bleu.grid(row=4 ,column=1, padx=8)
-bouton_vert.grid(row=4 ,column=2, padx=8)
-bouton_rose.grid(row=4 ,column=3, padx=8)
-bouton_orange.grid(row=4 ,column=4, padx=8)
-bouton_jaune.grid(row=5 ,column=1,columnspan=2, pady=15)
-bouton_blanc.grid(row=5 ,column=2, columnspan=2, pady=15)
+bouton_rouge  = tk.Button(text="Rouge", font=bouton, bg="red", bd=0, height=3,
+                width= 15, activebackground='red')
+bouton_bleu   = tk.Button(text="Bleu", font=bouton, bg="blue", bd=0, height=3,
+                width= 15, activebackground='blue')
+bouton_vert   = tk.Button(text="Vert", font=bouton, bg="green", bd=0, height=3,
+                width= 15, activebackground='green')
+bouton_rose   = tk.Button(text="Rose", font=bouton, bg="pink", bd=0, height=3,
+                width= 15, activebackground='pink')
+bouton_orange = tk.Button(text="Orange", font=bouton, bg="orange", bd=0,
+                height=3, width= 15, activebackground='orange')
+bouton_jaune  = tk.Button(text="Jaune", font=bouton, bg="yellow", bd=0,
+                height=3, width= 15, activebackground='yellow')
+bouton_blanc  = tk.Button(text="Blanc", font=bouton, bg="white", bd=0,
+                height=3, width= 15)
 
 #boutons demarrer et reinitialiser
-bouton_demarrer   = tk.Button(text=" Démarrer ", bg='gray55', width=20, height=2, command=temps_restant)
-bouton_reinitaliser = tk.Button(text=" Réinitialiser ", bg='gray55', width=20, height=2, command=reinitialiser)
-bouton_demarrer.grid(row=5, rowspan=2, column=0, pady=50)
-bouton_reinitaliser.grid(row=5, rowspan=2, column=4)
+bouton_demarrer   = tk.Button(text=" Démarrer ", bg='gray55', width=20,
+                height=2, command=temps_restant, font=demarrer_reinitialiser,
+                activebackground='gray55')
+bouton_reinitaliser = tk.Button(text=" Réinitialiser ", bg='gray55', width=20,
+                height=2, command=reinitialiser, font=demarrer_reinitialiser,
+                activebackground='gray55')
+
+#positionnement des widgets
+texte1.pack()
+message_score.pack()
+message_temps.pack()
+
+    #zone d'affichage des mots
+mots.place(x=285, y=125)
+
+    #boutons couleurs
+bouton_rouge.place(x=50, y=250)
+bouton_bleu.place(x=200, y=250)
+bouton_vert.place(x=350, y=250)
+bouton_rose.place(x=500, y=250)
+bouton_orange.place(x=650, y=250)
+bouton_jaune.place(x=275, y=335)
+bouton_blanc.place(x=425,y=335)
+
+    #boutons demarrer et reinitialiser
+bouton_demarrer.place(x=10, y=370)
+bouton_reinitaliser.place(x=605, y=370)
+
+#messages d'informations
+
 
 
 
@@ -111,5 +134,5 @@ bouton_reinitaliser.grid(row=5, rowspan=2, column=4)
 
 ###############################################################
 #lancement de la fenetre principale
-
+racine.geometry('825x433')
 racine.mainloop()
